dd7164825563e5571fb5973dd044f97a0996bc02
Imasha-Amarasinghe <Imasha-Amarasinghe@users.noreply.github.com>
2025-03-21 22:09:15 +0530

    Code updated

--- App.js
+++ App.js
@@ -9,24 +9,6 @@ export default function App() {
   const [amountToAdd, setAmountToAdd] = useState({});
   const [completedGoals, setCompletedGoals] = useState(0);
 
-  const addGoal = () => {
-    if (newGoal && newTarget) {
-      setGoals([
-        ...goals,
-        {
-          id: Date.now(),
-          name: newGoal,
-          progress: 0,
-          target: Number(newTarget),
-          completed: false,
-          icon: getGoalIcon(newGoal),
-        },
-      ]);
-      setNewGoal("");
-      setNewTarget("");
-    }
-  };
-
   const addMoney = (id) => {
     const amount = Number(amountToAdd[id]) || 0;
     setGoals((prevGoals) =>
